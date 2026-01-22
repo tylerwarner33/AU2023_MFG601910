f20cb5feecac22a31f26dc22dbfc8c3c337bc3e0
Tyler Warner <twarner@rand.com>
2026-01-22 07:04:38 -0600

    Include 'External Rules' folder in the template projects.

--- Templates/Basic Template/Utility Tools/PictureConverter.vb
+++ Templates/Basic Template/Utility Tools/PictureConverter.vb
@@ -1,69 +1,71 @@
 ﻿Imports System.Runtime.InteropServices
 
-
 Module PictureConverter
 
-    ' Class used to convert bitmaps and icons from their .Net native types into
-    ' an IPictureDisp object which is what the Inventor API requires. A typical
-    ' usage is shown below where MyIcon is a bitmap or icon that's available
-    ' as a resource of the project.
-    '
-
+    ''' <summary>
+    ''' Class used to convert bitmaps and icons from their .Net native types into an <see cref="stdole.IPictureDisp"/> object which is what the Inventor API requires.
+    ''' A typical usage is shown below where MyIcon is a bitmap or icon that's available as a resource of the project.
+    ''' </summary>
     Public NotInheritable Class PictureDispConverter
+
         <DllImport("OleAut32.dll", EntryPoint:="OleCreatePictureIndirect", ExactSpelling:=True, PreserveSig:=False)>
         Private Shared Function OleCreatePictureIndirect(
-             <MarshalAs(UnmanagedType.IUnknown)> ByVal picdesc As Object,
-             ByRef iid As Guid,
-             <MarshalAs(UnmanagedType.Bool)> ByVal fOwn As Boolean) As stdole.IPictureDisp
+            ByRef picdesc As PICTDESC_BITMAP,
+            ByRef iid As Guid,
+            <MarshalAs(UnmanagedType.Bool)> ByVal fOwn As Boolean) As stdole.IPictureDisp
+        End Function
+
+        <DllImport("OleAut32.dll", EntryPoint:="OleCreatePictureIndirect", ExactSpelling:=True, PreserveSig:=False)>
+        Private Shared Function OleCreatePictureIndirect(
+            ByRef picdesc As PICTDESC_ICON,
+            ByRef iid As Guid,
+            <MarshalAs(UnmanagedType.Bool)> ByVal fOwn As Boolean) As stdole.IPictureDisp
         End Function
 
         Shared iPictureDispGuid As Guid = GetType(stdole.IPictureDisp).GUID
 
-        Private NotInheritable Class PICTDESC
-            Private Sub New()
+        ' Picture Types
+        Private Const PICTYPE_BITMAP As Short = 1
+        Private Const PICTYPE_ICON As Short = 3
+
+        <StructLayout(LayoutKind.Sequential)>
+        Private Structure PICTDESC_BITMAP
+            Public cbSizeOfStruct As Integer
+            Public picType As Integer
+            Public hbitmap As IntPtr
+            Public hpal As IntPtr
+
+            Public Sub New(bmp As System.Drawing.Bitmap)
+                cbSizeOfStruct = Marshal.SizeOf(GetType(PICTDESC_BITMAP))
+                picType = PICTYPE_BITMAP
+                hbitmap = bmp.GetHbitmap()
+                hpal = IntPtr.Zero
             End Sub
+        End Structure
 
-            'Picture Types
-            Public Const PICTYPE_BITMAP As Short = 1
-            Public Const PICTYPE_ICON As Short = 3
+        <StructLayout(LayoutKind.Sequential)>
+        Private Structure PICTDESC_ICON
+            Public cbSizeOfStruct As Integer
+            Public picType As Integer
+            Public hicon As IntPtr
 
-            <StructLayout(LayoutKind.Sequential)>
-            Public Class Icon
-                Friend cbSizeOfStruct As Integer = Marshal.SizeOf(GetType(PICTDESC.Icon))
-                Friend picType As Integer = PICTDESC.PICTYPE_ICON
-                Friend hicon As IntPtr = IntPtr.Zero
-                Friend unused1 As Integer
-                Friend unused2 As Integer
+            Public Sub New(ico As System.Drawing.Icon)
+                cbSizeOfStruct = Marshal.SizeOf(GetType(PICTDESC_ICON))
+                picType = PICTYPE_ICON
+                hicon = ico.Handle
+            End Sub
+        End Structure
 
-                Friend Sub New(ByVal icon As System.Drawing.Icon)
-                    Me.hicon = icon.ToBitmap().GetHicon()
-                End Sub
-            End Class
 
-            <StructLayout(LayoutKind.Sequential)>
-            Public Class Bitmap
-                Friend cbSizeOfStruct As Integer = Marshal.SizeOf(GetType(PICTDESC.Bitmap))
-                Friend picType As Integer = PICTDESC.PICTYPE_BITMAP
-                Friend hbitmap As IntPtr = IntPtr.Zero
-                Friend hpal As IntPtr = IntPtr.Zero
-                Friend unused As Integer
-
-                Friend Sub New(ByVal bitmap As System.Drawing.Bitmap)
-                    Me.hbitmap = bitmap.GetHbitmap()
-                End Sub
-            End Class
-        End Class
-
-        Public Shared Function ToIPictureDisp(ByVal icon As System.Drawing.Icon) As stdole.IPictureDisp
-            Dim pictIcon As New PICTDESC.Icon(icon)
-            Return OleCreatePictureIndirect(pictIcon, iPictureDispGuid, True)
+        Public Shared Function ToIPictureDisp(bmp As System.Drawing.Bitmap) As stdole.IPictureDisp
+            Dim desc As New PICTDESC_BITMAP(bmp)
+            Return OleCreatePictureIndirect(desc, iPictureDispGuid, False)
         End Function
 
-        Public Shared Function ToIPictureDisp(ByVal bmp As System.Drawing.Bitmap) As stdole.IPictureDisp
-            Dim pictBmp As New PICTDESC.Bitmap(bmp)
-            Return OleCreatePictureIndirect(pictBmp, iPictureDispGuid, True)
+        Public Shared Function ToIPictureDisp(ico As System.Drawing.Icon) As stdole.IPictureDisp
+            Dim desc As New PICTDESC_ICON(ico)
+            Return OleCreatePictureIndirect(desc, iPictureDispGuid, False)
         End Function
-
 
     End Class
 
